a056956c2a97f2dde67b7c2713e22b207be60ee1
wngls <91951257+wngls@users.noreply.github.com>
2021-11-30 16:25:21 +0900

    Update lab 2-5.sh

--- codes/lab 2-5.sh
+++ codes/lab 2-5.sh
@@ -2,13 +2,13 @@
 
 echo "Starting the project"
 
-func_test()
+func()
 {
     echo "You are inside the function"
     echo `ls $1` #to command a commandword use ``
     echo "Function ended"
 }
 
-func_test $1
+func $1
 
 exit 0
